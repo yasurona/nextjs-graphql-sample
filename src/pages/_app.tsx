c40ffd729754fb5b31e112256768e86ad8ea3d66
yasurona <seiju4@gmail.com>
2022-10-16 01:57:57 +0900

    add: apollo

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,14 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { ApolloProvider } from '@apollo/client'
+import { csrClient } from '../lib/apollo'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ApolloProvider client={csrClient}>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  )
 }
 
 export default MyApp
